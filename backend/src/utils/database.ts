93e5a6d3c5bb79f38a0d5bcc1e73c345b6c54d37
RafaelSavoy <rafaelsavoy1@outlook.com>
2023-02-17 18:00:02 -0300

    Adding database and server configs

---
+++ backend/src/utils/database.ts
@@ -0,0 +1,12 @@
+import { createPool, Pool } from 'mysql2/promise';
+import { DB_HOST, DB_NAME, DB_PASSWORD, DB_USER } from './config';
+
+export const pool: Pool = createPool({
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
