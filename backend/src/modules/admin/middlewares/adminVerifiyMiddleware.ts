be88003432dd0243bbbca39122f2aa1e6e513273
RafaelSavoy <rafaelsavoy1@outlook.com>
2023-02-24 19:45:34 -0300

    Adding json to response

--- backend/src/modules/admin/middlewares/adminVerifiyMiddleware.ts
+++ backend/src/modules/admin/middlewares/adminVerifiyMiddleware.ts
@@ -8,7 +8,7 @@ export async function verifyPermissionMiddleware(
 ) {
   const token = req.headers.authorization?.split(' ')[1];
   if (!token) {
-    return res.status(401).send({ msg: 'Token necessário para essa ação' });
+    return res.status(401).json({ msg: 'Token necessário para essa ação' });
   }
   const response = await authServices.validate(token);
   if (response.userData.role !== 'admin') {
